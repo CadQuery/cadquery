779e5c5f1608db1db013a9ffd6da66de8a73e605
Dave Cowden <dave.cowden@gmail.com>
2014-05-24 13:34:30 -0400

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cadquery',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/dcowden/cadquery',
     license='LGPL',
     author='David Cowden',
