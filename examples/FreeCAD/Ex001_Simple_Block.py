4e5dfa3a46d7e09d3dfbaac8d5a9e82c1eed9788
Jeremy Wright <jwright@innovationsts.com>
2014-06-10 22:24:05 -0400

    Moved code of Ex001 example to the base so you don't have to call build.

--- examples/FreeCAD/Ex001_Simple_Block.py
+++ examples/FreeCAD/Ex001_Simple_Block.py
@@ -13,11 +13,9 @@ import sys
 #import sys
 #sys.path.append('/home/user/Downloads/cadquery/examples/FreeCAD')
 #import Ex001_Simple_Block
-#Ex001_Simple_Block.build()
 
 #If you need to reload the part after making a change, you can use the following lines within the FreeCAD console.
 #reload(Ex001_Simple_Block)
-#Ex001_Simple_Block.build()
 
 #You'll need to delete the original shape that was created, and the new shape should be named sequentially (Shape001, etc).
 
@@ -27,19 +25,18 @@ import sys
 
 import Part
 
-def build():	
-	#The dimensions of the box. These can be modified rather than changing the box's code directly.
-	length = 100.0
-	height = 100.0
-	thickness = 20.0
+#The dimensions of the box. These can be modified rather than changing the box's code directly.
+length = 100.0
+height = 100.0
+thickness = 20.0
 
-	#Create a 3D box based 
-	bb = cadquery.Workplane("XY").box(length, height, thickness)
+#Create a 3D box based 
+bb = cadquery.Workplane("XY").box(length, height, thickness)
 
-	#Get a cadquery solid object
-	solid = bb.val()
+#Get a cadquery solid object
+solid = bb.val()
 
-	#Use the wrapped property of a cadquery primitive to get a FreeCAD solid
-	Part.show(solid.wrapped)
+#Use the wrapped property of a cadquery primitive to get a FreeCAD solid
+Part.show(solid.wrapped)
 
-	#Would like to zoom to fit the part here, but FreeCAD doesn't seem to have that scripting functionality
+#Would like to zoom to fit the part here, but FreeCAD doesn't seem to have that scripting functionality
