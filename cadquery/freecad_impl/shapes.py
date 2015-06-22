807aa5d604bee38510c6c1ac91f3eeaebf630fd0
hyOzd <hy@ozderya.net>
2015-06-22 21:09:56 +0300

    use 'length' for chamfer instead of 'radius'

--- cadquery/freecad_impl/shapes.py
+++ cadquery/freecad_impl/shapes.py
@@ -812,19 +812,20 @@ class Solid(Shape):
         nativeEdges = [e.wrapped for e in edgeList]
         return Shape.cast(self.wrapped.makeFillet(radius, nativeEdges))
 
-    def chamfer(self, radius, radius2, edgeList):
+    def chamfer(self, length, length2, edgeList):
         """
         Chamfers the specified edges of this solid.
-        :param radius: radius > 0, the radius (length) of the chamfer
-        :param radius2: radius2 > 0, optional parameter for asymmetrical chamfer. Should be `None` if not required.
+        :param length: length > 0, the length (length) of the chamfer
+        :param length2: length2 > 0, optional parameter for asymmetrical chamfer. Should be `None` if not required.
         :param edgeList:  a list of Edge objects, which must belong to this solid
         :return: Chamfered solid
         """
         nativeEdges = [e.wrapped for e in edgeList]
-        if radius2:
-            return Shape.cast(self.wrapped.makeChamfer(radius, radius2, nativeEdges))
+        # note: we prefer 'length' word to 'radius' as opposed to FreeCAD's API
+        if length2:
+            return Shape.cast(self.wrapped.makeChamfer(length, length2, nativeEdges))
         else:
-            return Shape.cast(self.wrapped.makeChamfer(radius, nativeEdges))
+            return Shape.cast(self.wrapped.makeChamfer(length, nativeEdges))
 
     def shell(self, faceList, thickness, tolerance=0.0001):
         """
