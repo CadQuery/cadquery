3186b68b058069a7f7a667fcd11c565a3568b284
Youbao Zhang <zhangyoubao@siom.ac.cn>
2015-12-30 14:08:49 +0000

    1. Add a mirror function into CQ class(wrap) and Shape class(implement); 2. To get precise BoundBox, add tessellate(0.000001) in the BoundBox function implement......

--- cadquery/freecad_impl/shapes.py
+++ cadquery/freecad_impl/shapes.py
@@ -185,7 +185,21 @@ class Shape(object):
         return self.wrapped.isValid()
 
     def BoundingBox(self):
+	self.wrapped.tessellate(0.000001)
         return BoundBox(self.wrapped.BoundBox)
+
+    def mirror(self, mirrorPlane="XY", basePointVector=(0, 0, 0)):
+	if mirrorPlane == "XY" or mirrorPlane== "YX":
+	    mirrorPlaneNormalVector = FreeCAD.Base.Vector(0, 0, 1)
+	elif mirrorPlane == "XZ" or mirrorPlane == "ZX":
+	    mirrorPlaneNormalVector = FreeCAD.Base.Vector(0, 1, 0)
+	elif mirrorPlane == "YZ" or mirrorPlane == "ZY":
+	    mirrorPlaneNormalVector = FreeCAD.Base.Vector(1, 0, 0)
+
+        if type(basePointVector) == tuple:
+            basePointVector = Vector(basePointVector)
+
+	return Shape.cast(self.wrapped.mirror(basePointVector.wrapped, mirrorPlaneNormalVector))
 
     def Center(self):
         # A Part.Shape object doesn't have the CenterOfMass function, but it's wrapped Solid(s) does
