7f3c55fdcbb2911a2005eb7a1142048c114142d7
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2018-02-03 23:22:46 +0100

    Fixed some CQGI tests

--- tests/TestCQGI.py
+++ tests/TestCQGI.py
@@ -103,7 +103,7 @@ class TestCQGI(BaseTest):
         result = model.build({})
         self.assertFalse(result.success)
         self.assertIsNotNone(result.exception)
-        self.assertTrue(result.exception.message == "ERROR")
+        self.assertTrue(result.exception.args[0] == "ERROR")
 
     def test_that_invalid_syntax_in_script_fails_immediately(self):
         badscript = textwrap.dedent(
@@ -115,7 +115,7 @@ class TestCQGI(BaseTest):
         with self.assertRaises(Exception) as context:
             model = cqgi.CQModel(badscript)
 
-        self.assertTrue('invalid syntax' in context.exception)
+        self.assertTrue('invalid syntax' in context.exception.args)
 
     def test_that_two_results_are_returned(self):
         script = textwrap.dedent(
