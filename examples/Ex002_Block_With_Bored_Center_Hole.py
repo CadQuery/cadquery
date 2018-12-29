978e7f5703bca581b944e827bd1b8a396c9dfbc5
Jeremy Mack Wright <wrightjmf@gmail.com>
2018-12-29 18:44:08 -0500

    Reworked the core examples to exclude contributed examples.

---
+++ examples/Ex002_Block_With_Bored_Center_Hole.py
@@ -0,0 +1,20 @@
+import cadquery as cq
+
+# These can be modified rather than hardcoding values for each dimension.
+length = 80.0               # Length of the block
+height = 60.0               # Height of the block
+thickness = 10.0            # Thickness of the block
+center_hole_dia = 22.0      # Diameter of center hole in block
+
+# Create a block based on the dimensions above and add a 22mm center hole.
+# 1.  Establishes a workplane that an object can be built on.
+# 1a. Uses the X and Y origins to define the workplane, meaning that the
+# positive Z direction is "up", and the negative Z direction is "down".
+# 2.  The highest (max) Z face is selected and a new workplane is created on it.
+# 3.  The new workplane is used to drill a hole through the block.
+# 3a. The hole is automatically centered in the workplane.
+result = cq.Workplane("XY").box(length, height, thickness) \
+    .faces(">Z").workplane().hole(center_hole_dia)
+
+# Displays the result of this script
+show_object(result)
---
+++ examples/Ex014_Offset_Workplanes.py
@@ -0,0 +1,20 @@
+import cadquery as cq
+
+# 1.  Establishes a workplane that an object can be built on.
+# 1a. Uses the named plane orientation "front" to define the workplane, meaning
+#     that the positive Z direction is "up", and the negative Z direction
+#     is "down".
+# 2.  Creates a 3D box that will have geometry based off it later.
+result = cq.Workplane("front").box(3, 2, 0.5)
+
+# 3.  The lowest face in the X direction is selected with the <X selector.
+# 4. A new wokrplane is created
+# 4a.The workplane is offset from the object surface so that it is not touching
+#    the original box.
+result = result.faces("<X").workplane(offset=0.75)
+
+# 5. Creates a thin disc on the offset workplane that is floating near the box.
+result = result.circle(1.0).extrude(0.5)
+
+# Displays the result of this script
+show_object(result)
