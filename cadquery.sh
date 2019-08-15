6b32e42348b787d27dd155fc3e51cef3ef921909
Nathanaël <roipoussiere@protonmail.com>
2019-08-15 12:50:38 +0200

    docker: add python3.7 tag

--- cadquery.sh
+++ cadquery.sh
@@ -5,12 +5,12 @@ STDIN=$(cat)
 if [ -z "$STDIN" ]; then
    echo "*** CadQuery2 Docker Image ***"
    echo ""
-   echo "Usage: docker run roipoussiere/cadquery2 [options]"
+   echo "Usage: docker run cadquery/cadquery [options]"
    echo "Examples:"
    echo "- read a CadQuery script from stdin, write output 3d model svg to stdout:"
    echo "    echo \"import cadquery; print(cadquery.Workplane('XY').box(1,2,3).toSvg())\" > cadquery_script.py"
-   echo "    cat cadquery_script.py | docker run --rm -i roipoussiere/cadquery2 > 3d_model.svg"
+   echo "    cat cadquery_script.py | docker run --rm -i cadquery/cadquery > 3d_model.svg"
    exit 1
 else
-   exec python3 -c "$STDIN"
+   exec python -c "$STDIN"
 fi;
