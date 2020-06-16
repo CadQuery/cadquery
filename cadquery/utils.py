828cf9a1e6e8b1ec90f1abf6ee6bef3d9204ce47
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2020-06-16 19:18:20 +0200

    Clean-up and first pass on typing

--- cadquery/utils.py
+++ cadquery/utils.py
@@ -17,9 +17,21 @@ class deprecate_kwarg:
 
             if self.name not in f_sig_bound.kwargs:
                 warn(
-                    f"Default walue of {self.name} will change in next relase to {self.new_value}",
+                    f"Default walue of {self.name} will change in the next relase to {self.new_value}",
                     FutureWarning,
                 )
+
+            return f(*args, **kwargs)
+
+        return wrapped
+
+class deprecate:
+    
+    def __call__(self, f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+
+            warn(f"{f.__name__} will be removed in the next relase.",FutureWarning)
 
             return f(*args, **kwargs)
 
