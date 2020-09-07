3dc54d69e24d6242ea833e45771fcbae49cd56ac
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2020-09-07 12:25:30 +0200

    Use sphinx_autodoc_typehints

--- doc/conf.py
+++ doc/conf.py
@@ -35,10 +35,13 @@ import cadquery
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
     "cadquery.cq_directive",
 ]
+
+always_document_param_types = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
