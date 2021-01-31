edee3a7aa95a27131a014fca4ad34865183b2346
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2021-01-31 14:12:12 +0100

    Test examples from the docs too

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,16 +1,56 @@
 import pytest
 
+from glob import glob
+from itertools import chain
+
+from docutils.parsers.rst import directives, Directive
+from docutils.core import publish_doctree
+
+
 import cadquery as cq
 from cadquery import cqgi
-from glob import glob
+from cadquery.cq_directive import cq_directive
 
 
-@pytest.mark.parametrize("p", glob("examples/*.py"))
-def test_example(p):
+def find_examples(pattern="examples/*.py"):
 
-    # read the code
-    with open(p) as f:
-        code = f.read()
+    for p in glob(pattern):
+        with open(p) as f:
+            code = f.read()
+
+        yield code
+
+
+def find_examples_in_docs(pattern="doc/*.rst"):
+
+    # dummy CQ directive for code
+    class dummy_cq_directive(cq_directive):
+
+        codes = []
+
+        def run(self):
+
+            self.codes.append("\n".join(self.content))
+
+            return []
+
+    directives.register_directive("cadquery", dummy_cq_directive)
+
+    # read and parse all rst files
+    for p in glob(pattern):
+        with open(p) as f:
+            doc = f.read()
+
+        publish_doctree(doc)
+
+    # yield all code snippets
+    for c in dummy_cq_directive.codes:
+
+        yield c
+
+
+@pytest.mark.parametrize("code", chain(find_examples(), find_examples_in_docs()))
+def test_example(code):
 
     # build
     res = cqgi.parse(code).build()
