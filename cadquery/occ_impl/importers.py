4273331406b8981db51a5ae0dc49453772f07705
Greyson Christoforo <grey@christoforo.net>
2021-02-11 09:43:51 +0000

    force dxf layer name case agreement
    fixes #624

--- cadquery/occ_impl/importers.py
+++ cadquery/occ_impl/importers.py
@@ -222,12 +222,14 @@ def importDXF(filename, tol=1e-6, exclude=[]):
     :param tol: The tolerance used for merging edges into wires (default: 1e-6)
     :param exclude: a list of layer names not to import (default: [])
     """
+    
+    exclude_lwr = [ex.lower() for ex in exclude]
 
     dxf = ezdxf.readfile(filename)
     faces = []
 
     for name, layer in dxf.modelspace().groupby(dxfattrib="layer").items():
-        res = _dxf_convert(layer, tol) if name not in exclude else None
+        res = _dxf_convert(layer, tol) if name.lower() not in exclude_lwr else None
         if res:
             wire_sets = sortWiresByBuildOrder(res)
             for wire_set in wire_sets:
