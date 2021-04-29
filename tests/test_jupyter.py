e06518612f942570e4710e9db0ccbf1c5ed57f13
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2021-04-29 08:56:21 +0200

    Fixed tests

--- tests/test_jupyter.py
+++ tests/test_jupyter.py
@@ -1,14 +1,22 @@
 from tests import BaseTest
+from IPython.display import Javascript
 
 import cadquery
 
 
 class TestJupyter(BaseTest):
-    def test_repr_html(self):
+    def test_repr_javascript(self):
         cube = cadquery.Workplane("XY").box(1, 1, 1)
+        assy = cq.Assembly().add(cube)
         shape = cube.val()
+
         self.assertIsInstance(shape, cadquery.occ_impl.shapes.Solid)
 
-        # Test no exception on rendering to html
-        html = shape._repr_html_()
-        # TODO: verification improvement: test for valid html
+        # Test no exception on rendering to js
+        js1 = shape._repr_javascript_()
+        js2 = cube._repr_javascript_()
+        js3 = assy._repr_javascript_()
+
+        self.assertIsInstance(js1, Javascript)
+        self.assertIsInstance(js2, Javascript)
+        self.assertIsInstance(js3, Javascript)
