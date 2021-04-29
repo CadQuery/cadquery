26bf5f1f5fb5903f7ccd5b4258e9853c98d8a105
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2021-04-29 18:17:35 +0200

    Fixed tests

--- tests/test_jupyter.py
+++ tests/test_jupyter.py
@@ -1,16 +1,16 @@
 from tests import BaseTest
 from IPython.display import Javascript
 
-import cadquery
+import cadquery as cq
 
 
 class TestJupyter(BaseTest):
     def test_repr_javascript(self):
-        cube = cadquery.Workplane("XY").box(1, 1, 1)
+        cube = cq.Workplane("XY").box(1, 1, 1)
         assy = cq.Assembly().add(cube)
         shape = cube.val()
 
-        self.assertIsInstance(shape, cadquery.occ_impl.shapes.Solid)
+        self.assertIsInstance(shape, cq.occ_impl.shapes.Solid)
 
         # Test no exception on rendering to js
         js1 = shape._repr_javascript_()
