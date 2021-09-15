f76d58c4d02ee86e840c11b7bb5f28360302799e
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2021-09-15 08:59:25 +0200

    Add sketch tests

---
+++ tests/test_sketch.py
@@ -0,0 +1,34 @@
+from cadquery.sketch import Sketch, Vector
+
+from math import pi
+
+
+def test_assemble():
+
+    s1 = Sketch()
+    s1.segment((0.0, 0), (0.0, 2.0))
+    s1.segment(Vector(4.0, -1)).close().arc((0.7, 0.6), 0.4, 0.0, 360.0).assemble()
+
+
+def test_constraints():
+
+    s1 = (
+        Sketch()
+        .segment((0.0, 0), (0.0, 2.0), "s1")
+        .segment((0.5, 2.5), (1.0, 1), "s2")
+        .close("s3")
+    )
+    s1.constrain("s1", "Fixed", None)
+    s1.constrain("s1", "s2", "Coincident", None)
+    s1.constrain("s2", "s3", "Coincident", None)
+    s1.constrain("s3", "s1", "Coincident", None)
+    s1.constrain("s3", "s1", "Angle", pi / 2)
+    s1.constrain("s2", "s3", "Angle", pi - pi / 4)
+
+    s1.solve()
+
+    assert s1._solve_status["status"] == 4
+
+    s1.assemble()
+
+    assert s1._faces.isValid()
