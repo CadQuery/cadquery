a89933476182537e3a378d6c35525250ca454b56
Jeremy Wright <wrightjmf@gmail.com>
2023-03-21 10:46:14 -0400

    Fix black check

--- cadquery/assembly.py
+++ cadquery/assembly.py
@@ -467,9 +467,7 @@ class Assembly(object):
 
         # Make sure the export mode setting is correct
         if exportMode not in get_args(STEPExportModeLiterals):
-            raise ValueError(
-                f'Unknown assembly export mode {exportMode} for STEP'
-            )
+            raise ValueError(f"Unknown assembly export mode {exportMode} for STEP")
 
         if exportType is None:
             t = path.split(".")[-1].upper()
