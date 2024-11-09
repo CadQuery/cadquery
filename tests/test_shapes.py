f60ea782451df8a861f80dbf5618523ed5749773
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2024-11-09 14:55:20 +0100

    Some tests

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -6,6 +6,9 @@ from cadquery.occ_impl.shapes import (
     box,
     Solid,
     compound,
+    circle,
+    plane,
+    torus,
 )
 
 from pytest import approx
@@ -61,3 +64,47 @@ def test_shells():
     assert s.outerShell().Area() == approx(6 * 4)
     assert len(s.innerShells()) == 1
     assert s.innerShells()[0].Area() == approx(6 * 1)
+
+
+def test_curvature():
+
+    r = 10
+
+    c = circle(r)
+    w = polyline((0, 0), (1, 0), (1, 1))
+
+    assert c.curvatureAt(0) == approx(1 / r)
+
+    curvatures = c.curvatures([0, 0.5])
+
+    assert approx(curvatures[0]) == curvatures[1]
+
+    assert w.curvatureAt(0) == approx(w.curvatureAt(0.5))
+
+
+def test_normals():
+
+    d1 = 10
+    d2 = 1
+
+    (t,) = torus(d1, d2).faces()
+
+    n1 = t.normalAt((d1, d2))
+    n2 = t.normalAt((d1 + d2, 0))
+
+    assert approx(n1.toTuple()) == (0, 0, 1)
+    assert approx(n2.toTuple()) == (1, 0, 0)
+
+    n2, n3 = t.normals([(d1, d2), (d1 + d2, 0)])
+
+    assert approx(n2.toTuple()) == (0, 0, 1)
+    assert approx(n3.toTuple()) == (1, 0, 0)
+
+
+def test_trimming():
+
+    e = segment((0, 0), (0, 1))
+    f = plane(1, 1)
+
+    assert e.trim(0, 0.5).Length() == approx(e.Lengnth() / 2)
+    assert f.trim(0, 0.5, -0.5, 0.5).Area() == approx(f.Area() / 2)
