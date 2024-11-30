927581caa51cb412aa4f823a1950ed93ac06cc9e
AU <adam-urbanczyk@users.noreply.github.com>
2024-11-30 15:37:20 +0100

    Add explicit overloads to loft

--- cadquery/occ_impl/shapes.py
+++ cadquery/occ_impl/shapes.py
@@ -5561,6 +5561,36 @@ def sweep(
     return _compound_or_shape(results)
 
 
+@overload
+def loft(
+    s: Sequence[Shape],
+    cap: bool = False,
+    ruled: bool = False,
+    continuity: Literal["C1", "C2", "C3"] = "C2",
+    parametrization: Literal["uniform", "chordal", "centripetal"] = "uniform",
+    degree: int = 3,
+    compat: bool = True,
+    smoothing: bool = False,
+    weights: Tuple[float, float, float] = (1, 1, 1),
+) -> Shape:
+    ...
+
+
+@overload
+def loft(
+    *s: Shape,
+    cap: bool = False,
+    ruled: bool = False,
+    continuity: Literal["C1", "C2", "C3"] = "C2",
+    parametrization: Literal["uniform", "chordal", "centripetal"] = "uniform",
+    degree: int = 3,
+    compat: bool = True,
+    smoothing: bool = False,
+    weights: Tuple[float, float, float] = (1, 1, 1),
+) -> Shape:
+    ...
+
+
 @multimethod
 def loft(
     s: Sequence[Shape],
