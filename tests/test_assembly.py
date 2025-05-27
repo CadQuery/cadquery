9b48507e5341ecb4d9cc6edb30a8e55c6dbf7acf
Jeremy Wright <wrightjmf@gmail.com>
2025-05-27 16:01:19 -0400

    Black fix

--- tests/test_assembly.py
+++ tests/test_assembly.py
@@ -842,11 +842,21 @@ def test_assembly_step_import(tmp_path_factory):
     # Check for appropriate part name
     assert imported_assy.children[0].name == "cube_1"
     # Check for approximate color match
-    assert pytest.approx(imported_assy.children[0].color.toTuple(), rel=0.01) == (0.0, 1.0, 0.0, 1.0)
+    assert pytest.approx(imported_assy.children[0].color.toTuple(), rel=0.01) == (
+        0.0,
+        1.0,
+        0.0,
+        1.0,
+    )
     # Check for appropriate part name
     assert imported_assy.children[1].name == "cyl_1"
     # Check for approximate color match
-    assert pytest.approx(imported_assy.children[1].color.toTuple(), rel=0.01) == (0.0, 0.0, 1.0, 1.0)
+    assert pytest.approx(imported_assy.children[1].color.toTuple(), rel=0.01) == (
+        0.0,
+        0.0,
+        1.0,
+        1.0,
+    )
     assert imported_assy.name == "top-level"
 
     # Test a STEP file that does not contain an assembly
