578fd60060f01c3930da97b78200327d6fb71b41
Jeremy Wright <wrightjmf@gmail.com>
2025-06-03 09:16:44 -0400

    Refactored a bit to support nested assemblies better in the future

--- cadquery/occ_impl/importers/assembly.py
+++ cadquery/occ_impl/importers/assembly.py
@@ -50,6 +50,89 @@ def importStep(assy: AssemblyProtocol, path: str):
     color_tool = XCAFDoc_DocumentTool.ColorTool_s(doc.Main())
     layer_tool = XCAFDoc_DocumentTool.LayerTool_s(doc.Main())
 
+    def _process_simple_shape(label, parent_location=None):
+        shape = shape_tool.GetShape_s(label)
+
+        # Tracks the RGB color value and whether or not it was found
+        cq_color = None
+
+        # Load the name of the part in the assembly, if it is present
+        name = None
+        name_attr = TDataStd_Name()
+        label.FindAttribute(TDataStd_Name.GetID_s(), name_attr)
+        name = str(name_attr.Get().ToExtString())
+
+        # Process the color for the shape, which could be of different types
+        color = Quantity_Color()
+        cq_color = cq.Color(0.0, 0.0, 0.0)
+        if color_tool.GetColor_s(label, XCAFDoc_ColorSurf, color):
+            r = color.Red()
+            g = color.Green()
+            b = color.Blue()
+            cq_color = cq.Color(r, g, b)
+
+        # Handle the location if it was passed down form a parent component
+        if parent_location is not None:
+            assy.add(
+                cq.Shape.cast(shape), name=name, color=cq_color, loc=parent_location
+            )
+        else:
+            assy.add(cq.Shape.cast(shape), name=name, color=cq_color)
+
+        # Check all the attributes of all the children to find the subshapes and any names
+        for j in range(label.NbChildren()):
+            child_label = label.FindChild(j + 1)
+            attr_iterator = TDF_AttributeIterator(child_label)
+            while attr_iterator.More():
+                current_attr = attr_iterator.Value()
+
+                # Get the type name of the attribute so that we can decide how to handle it
+                if current_attr.DynamicType().Name() == "TNaming_NamedShape":
+                    # Save the shape so that we can add it to the subshape data
+                    cur_shape = current_attr.Get()
+
+                    # Find the layer name, if there is one set for this shape
+                    layers = TDF_LabelSequence()
+                    layer_tool.GetLayers(child_label, layers)
+                    for i in range(1, layers.Length() + 1):
+                        lbl = layers.Value(i)
+                        name_attr = TDataStd_Name()
+                        lbl.FindAttribute(TDataStd_Name.GetID_s(), name_attr)
+
+                        # Extract the layer name for the shape here
+                        layer_name = name_attr.Get().ToExtString()
+
+                        # Add the layer as a subshape entry on the assembly
+                        assy.addSubshape(cur_shape, layer=layer_name)
+
+                    # Find the subshape color, if there is one set for this shape
+                    color = Quantity_ColorRGBA()
+                    # Extract the color, if present on the shape
+                    if color_tool.GetColor(cur_shape, XCAFDoc_ColorSurf, color):
+                        rgb = color.GetRGB()
+                        cq_color = cq.Color(
+                            rgb.Red(), rgb.Green(), rgb.Blue(), color.Alpha()
+                        )
+
+                        # Save the color info via the assembly subshape mechanism
+                        assy.addSubshape(cur_shape, color=cq_color)
+                elif current_attr.DynamicType().Name() == "XCAFDoc_GraphNode":
+                    # Step up one level to try to get the name from the parent
+                    lbl = current_attr.GetFather(1).Label()
+
+                    # Step through and search for the name attribute
+                    it = TDF_AttributeIterator(lbl)
+                    while it.More():
+                        new_attr = it.Value()
+                        if new_attr.DynamicType().Name() == "TDataStd_Name":
+                            # Save this as the name of the subshape
+                            assy.addSubshape(
+                                cur_shape, name=new_attr.Get().ToExtString(),
+                            )
+                        it.Next()
+
+                attr_iterator.Next()
+
     def process_label(label, parent_location=None):
         """
         Recursive function that allows us to process the hierarchy of the assembly as represented
@@ -61,130 +144,42 @@ def importStep(assy: AssemblyProtocol, path: str):
             ref_label = TDF_Label()
             shape_tool.GetReferredShape_s(label, ref_label)
             process_label(ref_label, parent_location)
+
+            # Load the top-level name of the assembly, if it is present
+            name_attr = TDataStd_Name()
+            labels.Value(1).FindAttribute(TDataStd_Name.GetID_s(), name_attr)
+            assy.name = str(name_attr.Get().ToExtString())
+
             return
 
-        # Process components
-        comp_labels = TDF_LabelSequence()
-        shape_tool.GetComponents_s(label, comp_labels)
-        for i in range(comp_labels.Length()):
-            sub_label = comp_labels.Value(i + 1)
+        # See if this is an assembly (or sub-assembly)
+        if shape_tool.IsAssembly_s(label):
+            # Recursively process its components (children)
+            comp_labels = TDF_LabelSequence()
+            shape_tool.GetComponents_s(label, comp_labels)
+            for i in range(comp_labels.Length()):
+                sub_label = comp_labels.Value(i + 1)
 
-            # The component level holds the location for its shapes
-            location = parent_location
-            loc = shape_tool.GetLocation_s(sub_label)
-            location = cq.Location((0.0, 0.0, 0.0))
-            if loc:
-                location = cq.Location(loc)
+                # Pass down the location or other context as needed
+                process_label(sub_label, parent_location)
+            return
 
-                # Make sure that the location object is actually doing something interesting
-                # This is done because the location may have to go through multiple levels of
-                # components before the shapes are found. This allows the top-level component
-                # to specify the location/rotation of the shapes.
-                if location.toTuple()[0] == (0, 0, 0) and location.toTuple()[1] == (
-                    0,
-                    0,
-                    0,
-                ):
-                    location = parent_location
-
-            process_label(sub_label, location)
-
-        # Check to see if we have an endpoint shape
+        # Check to see if we have an endpoint shape and process it
         if shape_tool.IsSimpleShape_s(label):
-            shape = shape_tool.GetShape_s(label)
+            _process_simple_shape(label, parent_location)
 
-            # Tracks the RGB color value and whether or not it was found
-            cq_color = None
-
-            # Load the name of the part in the assembly, if it is present
-            name = None
-            name_attr = TDataStd_Name()
-            label.FindAttribute(TDataStd_Name.GetID_s(), name_attr)
-            name = str(name_attr.Get().ToExtString())
-
-            # Process the color for the shape, which could be of different types
-            color = Quantity_Color()
-            cq_color = cq.Color(0.0, 0.0, 0.0)
-            if color_tool.GetColor_s(label, XCAFDoc_ColorSurf, color):
-                r = color.Red()
-                g = color.Green()
-                b = color.Blue()
-                cq_color = cq.Color(r, g, b)
-
-            # Handle the location if it was passed down form a parent component
-            if parent_location is not None:
-                assy.add(
-                    cq.Shape.cast(shape), name=name, color=cq_color, loc=parent_location
-                )
-            else:
-                assy.add(cq.Shape.cast(shape), name=name, color=cq_color)
-
-            # Check all the attributes of all the children to find the subshapes and any names
-            for j in range(label.NbChildren()):
-                child_label = label.FindChild(j + 1)
-                attr_iterator = TDF_AttributeIterator(child_label)
-                while attr_iterator.More():
-                    current_attr = attr_iterator.Value()
-
-                    # Get the type name of the attribute so that we can decide how to handle it
-                    if current_attr.DynamicType().Name() == "TNaming_NamedShape":
-                        # Save the shape so that we can add it to the subshape data
-                        cur_shape = current_attr.Get()
-
-                        # Find the layer name, if there is one set for this shape
-                        layers = TDF_LabelSequence()
-                        layer_tool.GetLayers(child_label, layers)
-                        for i in range(1, layers.Length() + 1):
-                            lbl = layers.Value(i)
-                            name_attr = TDataStd_Name()
-                            lbl.FindAttribute(TDataStd_Name.GetID_s(), name_attr)
-
-                            # Extract the layer name for the shape here
-                            layer_name = name_attr.Get().ToExtString()
-
-                            # Add the layer as a subshape entry on the assembly
-                            assy.addSubshape(cur_shape, layer=layer_name)
-
-                        # Find the subshape color, if there is one set for this shape
-                        color = Quantity_ColorRGBA()
-                        # Extract the color, if present on the shape
-                        if color_tool.GetColor(cur_shape, XCAFDoc_ColorSurf, color):
-                            rgb = color.GetRGB()
-                            cq_color = cq.Color(
-                                rgb.Red(), rgb.Green(), rgb.Blue(), color.Alpha()
-                            )
-
-                            # Save the color info via the assembly subshape mechanism
-                            assy.addSubshape(cur_shape, color=cq_color)
-                    elif current_attr.DynamicType().Name() == "XCAFDoc_GraphNode":
-                        # Step up one level to try to get the name from the parent
-                        lbl = current_attr.GetFather(1).Label()
-
-                        # Step through and search for the name attribute
-                        it = TDF_AttributeIterator(lbl)
-                        while it.More():
-                            new_attr = it.Value()
-                            if new_attr.DynamicType().Name() == "TDataStd_Name":
-                                # Save this as the name of the subshape
-                                assy.addSubshape(
-                                    cur_shape, name=new_attr.Get().ToExtString(),
-                                )
-                            it.Next()
-
-                    attr_iterator.Next()
-
-    # Grab the labels, which should hold the assembly parent
+    # Look for the top-level assembly
+    found_top_level_assembly = False
     labels = TDF_LabelSequence()
     shape_tool.GetFreeShapes(labels)
+    for i in range(labels.Length()):
+        # Make sure that we have an assembly at the top level
+        if shape_tool.IsTopLevel(labels.Value(i + 1)):
+            if shape_tool.IsAssembly_s(labels.Value(i + 1)):
+                found_top_level_assembly = True
 
-    # Make sure that we are working with an assembly
-    if shape_tool.IsAssembly_s(labels.Value(1)):
-        # Start the recursive processing of the assembly
-        process_label(labels.Value(1))
+            process_label(labels.Value(i + 1))
 
-        # Load the top-level name of the assembly, if it is present
-        name_attr = TDataStd_Name()
-        labels.Value(1).FindAttribute(TDataStd_Name.GetID_s(), name_attr)
-        assy.name = str(name_attr.Get().ToExtString())
-    else:
+    # If we did not find a top-level assembly, raise an error
+    if not found_top_level_assembly:
         raise ValueError("Step file does not contain an assembly")
