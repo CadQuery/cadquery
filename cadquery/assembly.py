92949565167d5ce237062a9ea713c9ed1206234d
Jonathan Shimwell <mail@jshimwell.com>
2025-09-01 17:08:36 +0200

    adding name of duplicate name to error message

--- cadquery/assembly.py
+++ cadquery/assembly.py
@@ -218,7 +218,7 @@ class Assembly(object):
             # enforce unique names
             name = kwargs["name"] if kwargs.get("name") else arg.name
             if name in self.objects:
-                raise ValueError("Unique name is required")
+                raise ValueError(f"Unique name is required. {name} is already in the assembly")
 
             subassy = arg._copy()
 
