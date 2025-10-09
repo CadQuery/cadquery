871fd11326b172294aa15838fcbb99cfee11a9e1
Joschua-Conrad <32811308+Joschua-Conrad@users.noreply.github.com>
2025-10-09 10:33:13 +0200

    Comments on introduced test

--- tests/test_geom.py
+++ tests/test_geom.py
@@ -3,21 +3,26 @@ import pytest
 import itertools
 
 def test_Plane_from_Location():
+    
+    #Star from random lcation and plane
     originalloc = Location(x=3, y=5, z=6, rx=15, ry=25, rz=40)
     originalpl = Plane((3, 5, 6), (2, 4, 7), (9, 8, 1))
     
+    #Convert back and forth, such that comparable pairs are created
     plforth = Plane(originalloc)
     locforth = Location(originalpl)
-    
     locback = Location(plforth)
     plback = Plane(locforth)
     
+    #Create raw locations, which are flat tuples of raw numbers, suitable for
+    #assertion with pytes.approx
     locraws = list()
     for loc in (originalloc, locback):
         loc = loc.toTuple()
         loc = tuple(itertools.chain(*loc))
         locraws.append(loc)
         
+    #Same for planes
     plraws = list()
     for pl in (originalpl, plback):
         pl = (
@@ -29,5 +34,6 @@ def test_Plane_from_Location():
         pl = tuple(itertools.chain(*pl))
         plraws.append(pl)
     
+    #Perform assertions
     assert locraws[0] == pytest.approx(locraws[1])
     assert plraws[0] == pytest.approx(plraws[1])
