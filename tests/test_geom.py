aed6fb18ac128715a58ace4aee1fb75f5b1482e1
Joschua-Conrad <32811308+Joschua-Conrad@users.noreply.github.com>
2025-10-09 11:18:23 +0200

    Test now has only a single initial Plane to start conversiosn from

--- tests/test_geom.py
+++ tests/test_geom.py
@@ -4,20 +4,20 @@ import itertools
 
 def test_Plane_from_Location():
     
-    #Star from random lcation and plane
-    originalloc = Location(x=3, y=5, z=6, rx=15, ry=25, rz=40)
+    #Start from random Plane with classical __init__
     originalpl = Plane((3, 5, 6), (2, 4, 7), (9, 8, 1))
     
     #Convert back and forth, such that comparable pairs are created
-    plforth = Plane(originalloc)
+    #plforth = Plane(originalloc)
     locforth = Location(originalpl)
-    locback = Location(plforth)
+    #locback = Location(plforth)
     plback = Plane(locforth)
+    locback = Location(plback)
     
     #Create raw locations, which are flat tuples of raw numbers, suitable for
     #assertion with pytes.approx
     locraws = list()
-    for loc in (originalloc, locback):
+    for loc in (locforth, locback):
         loc = loc.toTuple()
         loc = tuple(itertools.chain(*loc))
         locraws.append(loc)
