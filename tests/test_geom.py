ba6abe4a33eb1087145abcbc45bd5d2abdd3c233
Joschua-Conrad <32811308+Joschua-Conrad@users.noreply.github.com>
2025-10-09 13:50:42 +0200

    Tests pass now, after enforcing that tested xDir and normal are orthogonal

--- tests/test_geom.py
+++ tests/test_geom.py
@@ -1,16 +1,60 @@
-from cadquery.occ_impl.shapes import Location, Plane
+from cadquery.occ_impl.shapes import Location, Plane, Vector
 import pytest
 import itertools
+import math
 
-def test_Plane_from_Location():
+#Use instead of 1 to create a vector with two non-zero elements and length 1
+twocomp = math.sqrt(1./2.)
+
+#Create different test cases from different initial plane.
+#Testing the different components is mainly useful for debugging if things
+#do not work.
+@pytest.mark.parametrize(
+    ["plargs",],
+    [
+        #Default plane
+        (((3, 5, 6),),),
+        #Just xDir specified
+        (((3, 5, 6), (1, 0, 0),),),
+        (((3, 5, 6), (0, 1, 0),),),
+        #xDir and normal specified
+        (((3, 5, 6), (1, 0, 0), (0, 0, 1),),),
+        (((3, 5, 6), (1, 0, 0), (0, 1, 0),),),
+        (((3, 5, 6), (0, 1, 0), (0, 0, 1),),),
+        (((3, 5, 6), (0, 1, 0), (1, 0, 0),),),
+        #JUst xDir, but with multiple vector components
+        (((3, 5, 6), (1, 1, 0),),),
+        (((3, 5, 6), (1, 0, 1),),),
+        (((3, 5, 6), (0, 1, 1),),),
+        #Vectors with random non-trivial directions
+        (((3, 5, 6), (2, 4, 7), (9, 8, 1),),),
+    ]
+)
+def test_Plane_from_Location(plargs):
+    #Test conversion between Plane and Location by converting multiple
+    #times between them, such that two Plane and two Location can be
+    #compared respectively.
+    
+    #If there are three things in plargs, ensure that xDir and normal are
+    #orthogonal. That should be ensured by an exception in Plane.__init__.
+    #This here makes the normal orthogonal to xDir by subtracting its
+    #projection on xDir.
+    #If no normal is given, the default normal is assumed.
+    if len(plargs) == 2:
+        plargs = (*plargs, (0, 0, 1))
+    if len(plargs) == 3:
+        origin, xDir, normal = plargs
+        xDir = Vector(xDir)
+        normal = Vector(normal)
+        normal -= normal.projectToLine(xDir)
+        plargs = (origin, xDir, normal)
     
     #Start from random Plane with classical __init__
-    originalpl = Plane((3, 5, 6), (2, 4, 7), (9, 8, 1))
+    originalpl = Plane(*plargs)
     
     #Convert back and forth, such that comparable pairs are created
     #plforth = Plane(originalloc)
     locforth = Location(originalpl)
-    #locback = Location(plforth)
     plback = Plane(locforth)
     locback = Location(plback)
     
