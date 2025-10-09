f3437f7eba5fb71b109c70939ec7aa0d6f285ca1
Joschua-Conrad <32811308+Joschua-Conrad@users.noreply.github.com>
2025-10-09 18:30:56 +0200

    Resolved all flake8 linting warnings: whitespace only

--- tests/test_geom.py
+++ tests/test_geom.py
@@ -2,7 +2,8 @@ from cadquery.occ_impl.shapes import Location, Plane, Vector
 import pytest
 import itertools
 
-#Conversion can betriggered from explicit constructor, or from property
+
+# Conversion can betriggered from explicit constructor, or from property
 @pytest.mark.parametrize(
     ["useproperty",],
     [
@@ -10,54 +11,54 @@ import itertools
         (True,),
     ]
 )
-#Create different test cases from different initial plane.
-#Testing the different components is mainly useful for debugging if things
-#do not work.
+# Create different test cases from different initial plane.
+# Testing the different components is mainly useful for debugging if things
+# do not work.
 @pytest.mark.parametrize(
     ["plargs",],
     [
-        #Default plane
+        # Default plane
         (((3, 5, 6),),),
-        #Just xDir specified, but never parallel to the default normal
+        # Just xDir specified, but never parallel to the default normal
         (((3, 5, 6), (1, 0, 0),),),
         (((3, 5, 6), (0, 1, 0),),),
-        #xDir and normal specified.
-        #Omit normals, that were included as default, and once which
-        #have no component orthogonal to xDir
+        # xDir and normal specified.
+        # Omit normals, that were included as default, and once which
+        # have no component orthogonal to xDir
         (((3, 5, 6), (1, 0, 0), (0, 1, 0),),),
         (((3, 5, 6), (0, 1, 0), (1, 0, 0),),),
-        #JUst xDir, but with multiple vector components
+        # JUst xDir, but with multiple vector components
         (((3, 5, 6), (1, 1, 0),),),
         (((3, 5, 6), (1, 0, 1),),),
         (((3, 5, 6), (0, 1, 1),),),
-        #Multiple components in xdir and normal
+        # Multiple components in xdir and normal
         (((3, 5, 6), (1, 1, 0), (1, 0, 1),),),
         (((3, 5, 6), (1, 1, 0), (0, 1, 1),),),
         (((3, 5, 6), (1, 0, 1), (1, 1, 0),),),
         (((3, 5, 6), (1, 0, 1), (0, 1, 1),),),
         (((3, 5, 6), (0, 1, 1), (1, 1, 0),),),
         (((3, 5, 6), (0, 1, 1), (1, 0, 1),),),
-        #Same, but introduce negative directions
+        # Same, but introduce negative directions
         (((3, 5, 6), (-1, 1, 0), (-1, 0, -1),),),
         (((3, 5, 6), (1, -1, 0), (0, -1, -1),),),
         (((3, 5, 6), (1, 0, -1), (1, -1, 0),),),
         (((3, 5, 6), (1, 0, -1), (0, -1, 1),),),
         (((3, 5, 6), (0, -1, -1), (-1, 1, 0),),),
         (((3, 5, 6), (0, -1, -1), (1, 0, -1),),),
-        #Vectors with random non-trivial directions
+        # Vectors with random non-trivial directions
         (((3, 5, 6), (2, 4, 7), (9, 8, 1),),),
     ]
 )
 def test_Plane_from_Location(plargs, useproperty):
-    #Test conversion between Plane and Location by converting multiple
-    #times between them, such that two Plane and two Location can be
-    #compared respectively.
-    
-    #If there are three things in plargs, ensure that xDir and normal are
-    #orthogonal. That should be ensured by an exception in Plane.__init__.
-    #This here makes the normal orthogonal to xDir by subtracting its
-    #projection on xDir.
-    #If no normal is given, the default normal is assumed.
+    # Test conversion between Plane and Location by converting multiple
+    # times between them, such that two Plane and two Location can be
+    # compared respectively.
+
+    # If there are three things in plargs, ensure that xDir and normal are
+    # orthogonal. That should be ensured by an exception in Plane.__init__.
+    # This here makes the normal orthogonal to xDir by subtracting its
+    # projection on xDir.
+    # If no normal is given, the default normal is assumed.
     if len(plargs) == 2:
         plargs = (*plargs, (0, 0, 1))
     if len(plargs) == 3:
@@ -66,12 +67,12 @@ def test_Plane_from_Location(plargs, useproperty):
         normal = Vector(normal)
         normal -= normal.projectToLine(xDir)
         plargs = (origin, xDir, normal)
-    
-    #Start from random Plane with classical __init__
+
+    # Start from random Plane with classical __init__
     originalpl = Plane(*plargs)
-    
-    #Convert back and forth, such that comparable pairs are created.
-    #Depending on test fixture, call constructor directly or use properties
+
+    # Convert back and forth, such that comparable pairs are created.
+    # Depending on test fixture, call constructor directly or use properties
     if useproperty:
         locforth = originalpl.location
         plback = locforth.plane
@@ -80,16 +81,16 @@ def test_Plane_from_Location(plargs, useproperty):
         locforth = Location(originalpl)
         plback = Plane(locforth)
         locback = Location(plback)
-    
-    #Create raw locations, which are flat tuples of raw numbers, suitable for
-    #assertion with pytes.approx
+
+    # Create raw locations, which are flat tuples of raw numbers, suitable for
+    # assertion with pytes.approx
     locraws = list()
     for loc in (locforth, locback):
         loc = loc.toTuple()
         loc = tuple(itertools.chain(*loc))
         locraws.append(loc)
-        
-    #Same for planes
+
+    # Same for planes
     plraws = list()
     for pl in (originalpl, plback):
         pl = (
@@ -100,7 +101,7 @@ def test_Plane_from_Location(plargs, useproperty):
         )
         pl = tuple(itertools.chain(*pl))
         plraws.append(pl)
-    
-    #Perform assertions
+
+    # Perform assertions
     assert locraws[0] == pytest.approx(locraws[1])
     assert plraws[0] == pytest.approx(plraws[1])
