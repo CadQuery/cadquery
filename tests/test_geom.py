607bb83d2d8e4b7e408d857e71ca2bbbe0832335
Joschua-Conrad <32811308+Joschua-Conrad@users.noreply.github.com>
2025-10-10 08:35:10 +0200

    More assertions on expected Location properites

--- tests/test_geom.py
+++ tests/test_geom.py
@@ -14,42 +14,47 @@ import itertools
 # Create different test cases from different initial plane.
 # Testing the different components is mainly useful for debugging if things
 # do not work.
+# Arguments to Plane.__init__ along with expected rotations in a converted
+# Location object are given here.
 @pytest.mark.parametrize(
-    ["plargs",],
+    ["plargs", "expectedrot"],
     [
         # Default plane
-        (((3, 5, 6),),),
+        (((3, 5, 6),), (0, 0, 0),),
         # Just xDir specified, but never parallel to the default normal
-        (((3, 5, 6), (1, 0, 0),),),
-        (((3, 5, 6), (0, 1, 0),),),
+        (((3, 5, 6), (1, 0, 0),), (0, 0, 0),),
+        (((3, 5, 6), (0, 1, 0),), (0, 0, 90),),
         # xDir and normal specified.
         # Omit normals, that were included as default, and once which
         # have no component orthogonal to xDir
-        (((3, 5, 6), (1, 0, 0), (0, 1, 0),),),
-        (((3, 5, 6), (0, 1, 0), (1, 0, 0),),),
+        (((3, 5, 6), (1, 0, 0), (0, 1, 0),), (-90, 0, 0),),
+        (((3, 5, 6), (0, 1, 0), (1, 0, 0),), (90, 0, 90),),
         # JUst xDir, but with multiple vector components
-        (((3, 5, 6), (1, 1, 0),),),
-        (((3, 5, 6), (1, 0, 1),),),
-        (((3, 5, 6), (0, 1, 1),),),
+        (((3, 5, 6), (1, 1, 0),), (0, 0, 45),),
+        (((3, 5, 6), (1, 0, 1),), (0, -45, 0),),
+        (((3, 5, 6), (0, 1, 1),), (0, -45, 90),),
         # Multiple components in xdir and normal
-        (((3, 5, 6), (1, 1, 0), (1, 0, 1),),),
-        (((3, 5, 6), (1, 1, 0), (0, 1, 1),),),
-        (((3, 5, 6), (1, 0, 1), (1, 1, 0),),),
-        (((3, 5, 6), (1, 0, 1), (0, 1, 1),),),
-        (((3, 5, 6), (0, 1, 1), (1, 1, 0),),),
-        (((3, 5, 6), (0, 1, 1), (1, 0, 1),),),
+        # Starting from here, there are no known golden Location rotations,
+        # as normal is made orthogonal to xDir and as rotational angles
+        # are non-trivial.
+        (((3, 5, 6), (1, 1, 0), (1, 0, 1),), None,),
+        (((3, 5, 6), (1, 1, 0), (0, 1, 1),), None,),
+        (((3, 5, 6), (1, 0, 1), (1, 1, 0),), None,),
+        (((3, 5, 6), (1, 0, 1), (0, 1, 1),), None,),
+        (((3, 5, 6), (0, 1, 1), (1, 1, 0),), None,),
+        (((3, 5, 6), (0, 1, 1), (1, 0, 1),), None,),
         # Same, but introduce negative directions
-        (((3, 5, 6), (-1, 1, 0), (-1, 0, -1),),),
-        (((3, 5, 6), (1, -1, 0), (0, -1, -1),),),
-        (((3, 5, 6), (1, 0, -1), (1, -1, 0),),),
-        (((3, 5, 6), (1, 0, -1), (0, -1, 1),),),
-        (((3, 5, 6), (0, -1, -1), (-1, 1, 0),),),
-        (((3, 5, 6), (0, -1, -1), (1, 0, -1),),),
+        (((3, 5, 6), (-1, 1, 0), (-1, 0, -1),), None,),
+        (((3, 5, 6), (1, -1, 0), (0, -1, -1),), None,),
+        (((3, 5, 6), (1, 0, -1), (1, -1, 0),), None,),
+        (((3, 5, 6), (1, 0, -1), (0, -1, 1),), None,),
+        (((3, 5, 6), (0, -1, -1), (-1, 1, 0),), None,),
+        (((3, 5, 6), (0, -1, -1), (1, 0, -1),), None,),
         # Vectors with random non-trivial directions
-        (((3, 5, 6), (2, 4, 7), (9, 8, 1),),),
+        (((3, 5, 6), (2, 4, 7), (9, 8, 1),), None,),
     ]
 )
-def test_Plane_from_Location(plargs, useproperty):
+def test_Plane_from_Location(plargs, expectedrot, useproperty):
     # Test conversion between Plane and Location by converting multiple
     # times between them, such that two Plane and two Location can be
     # compared respectively.
@@ -59,13 +64,21 @@ def test_Plane_from_Location(plargs, useproperty):
     # This here makes the normal orthogonal to xDir by subtracting its
     # projection on xDir.
     # If no normal is given, the default normal is assumed.
-    if len(plargs) == 2:
-        plargs = (*plargs, (0, 0, 1))
+    # Packed and unpacked arguments to Plane are kept the same.
+    if len(plargs) == 1:
+        origin, = plargs
+    elif len(plargs) == 2:
+        plargs = (*plargs, (0, 0, 1),)
+    # If len(plargs) was 2, it is now 3, and the normal still needs to be
+    # made orthogonal to xDir.
     if len(plargs) == 3:
         origin, xDir, normal = plargs
         xDir = Vector(xDir)
         normal = Vector(normal)
         normal -= normal.projectToLine(xDir)
+        xDir = xDir.toTuple()
+        normal = normal.toTuple()
+        plargs = (origin, xDir, normal,)
 
     # Start from random Plane with classical __init__
     # Use keyword arguments on purpose, as they still need to work after
@@ -88,7 +101,7 @@ def test_Plane_from_Location(plargs, useproperty):
         locback = Location(plback)
 
     # Create raw locations, which are flat tuples of raw numbers, suitable for
-    # assertion with pytes.approx
+    # assertion with pytest.approx
     locraws = list()
     for loc in (locforth, locback):
         loc = loc.toTuple()
@@ -107,6 +120,15 @@ def test_Plane_from_Location(plargs, useproperty):
         pl = tuple(itertools.chain(*pl))
         plraws.append(pl)
 
-    # Perform assertions
+    # Assert the properties of the location object.
+    # Asserting on one Location is enough, as equality to the other one is
+    # asserted below.
+    # First, its origin shall be the same
+    assert locraws[0][0:3] == pytest.approx(origin)
+    # Then rotations are asserted from manual values
+    if expectedrot is not None:
+        assert locraws[0][3:6] == pytest.approx(expectedrot)
+
+    # Assert that pairs of PLane or Location are equal after conversion
     assert locraws[0] == pytest.approx(locraws[1])
     assert plraws[0] == pytest.approx(plraws[1])
