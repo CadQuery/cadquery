7b186c3bafd62f64206d10fa40e4c2d17f8bcca7
Jeremy Wright <wrightjmf@gmail.com>
2025-10-28 06:06:35 -0400

    Updates for 2.6.1 release

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ if not is_rtd and not is_appveyor and not is_azure and not is_conda:
 
 setup(
     name="cadquery",
-    version="2.7-dev",
+    version="2.6.1",
     url="https://github.com/CadQuery/cadquery",
     license="Apache Public License 2.0",
     author="David Cowden",
