5f4c5e3dcedad00384a3de76d3c7825c324acc6a
adam-urbanczyk <13981538+adam-urbanczyk@users.noreply.github.com>
2026-01-19 21:42:34 +0100

    Fix Workplane

--- cadquery/cq.py
+++ cadquery/cq.py
@@ -3716,7 +3716,7 @@ class Workplane(object):
 
         for el in self.objects:
             if isinstance(el, Sketch):
-                rv.extend(el)
+                rv.extend(f for f in el if isinstance(f, Face))
             elif isinstance(el, Face):
                 rv.append(el)
             elif isinstance(el, Compound):
@@ -3736,7 +3736,7 @@ class Workplane(object):
 
         for el in self.objects:
             if isinstance(el, Sketch):
-                rv.extend(el)
+                rv.extend(f for f in el if isinstance(f, Face))
             elif isinstance(el, (Face, Vertex)):
                 rv.append(el)
             elif isinstance(el, Compound):
