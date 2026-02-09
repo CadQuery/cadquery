ed616e139d65d5a2dd030915b6d7eb911c3670ae
Matteo Scalia <mteosca@gmail.com>
2026-02-09 17:59:19 +0100

    black fix

--- tests/test_sketch.py
+++ tests/test_sketch.py
@@ -841,11 +841,7 @@ def test_equal_constraints():
     assert s2._tags["top_left"][0].radius() == approx(1)
     assert s2._tags["bottom_left"][0].radius() == approx(1)
 
-    s3 = (
-        Sketch()
-        .segment((-1, 0), (1, 0), "segment")
-        .arc((0, 0), 0.8, 0, 180, "arc")
-    )
+    s3 = Sketch().segment((-1, 0), (1, 0), "segment").arc((0, 0), 0.8, 0, 180, "arc")
     s3.constrain("segment", "Fixed", None)
     s3.constrain("arc", "FixedPoint", None)
     s3.constrain("arc", "ArcAngle", 180)
