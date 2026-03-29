Fix off by one bug for periodic surfaces and curves

cadquery/occ_impl/nurbs.py
@@ -339,7 +339,7 @@ class Surface(NamedTuple):
 
 @njiti
 def _preprocess(
-    u: Array, order: int, knots: Array, periodic: float
+    u: Array, order: int, knots: Array, periodic: bool
 ) -> Tuple[Array, Array, Optional[int], Optional[int], int]:
     """
     Helper for handling periodicity. This function extends the knot vector,
@@ -966,7 +966,7 @@ def designMatrix(u: Array, order: int, knots: Array, periodic: bool = False) -> 
         # update the matrix
         rv.i[i * n : (i + 1) * n] = i
         rv.j[i * n : (i + 1) * n] = (
-            span - order + np.arange(n)
+            span - order + int(periodic) + np.arange(n)
         ) % nb  # NB: this is due to periodicity
         rv.v[i * n : (i + 1) * n] = temp
 
@@ -1039,8 +1039,10 @@ def designMatrix2D(
         # update the matrix
         rv.i[i * nj : (i + 1) * nj] = i
         rv.j[i * nj : (i + 1) * nj] = (
-            ((uspan - uorder + 1 + np.arange(nu)) % nu_total) * nv_total
-            + ((vspan - vorder + 1 + np.arange(nv)) % nv_total)[:, np.newaxis]
+            ((uspan - uorder + int(uperiodic) + np.arange(nu)) % nu_total) * nv_total
+            + ((vspan - vorder + int(vperiodic) + np.arange(nv)) % nv_total)[
+                :, np.newaxis
+            ]
         ).ravel()
         rv.v[i * nj : (i + 1) * nj] = (utemp * vtemp[:, np.newaxis]).ravel()
 
@@ -1153,7 +1155,7 @@ def periodicDerMatrix(u: Array, order: int, dorder: int, knots: Array) -> list[C
         for di in range(dorder + 1):
             rv[di].i[i * n : (i + 1) * n] = i
             rv[di].j[i * n : (i + 1) * n] = (
-                span - order + np.arange(n)
+                span - order + 1 + np.arange(n)
             ) % nb  # NB: this is due to periodicity
             rv[di].v[i * n : (i + 1) * n] = temp[di, :]
 
@@ -1381,8 +1383,11 @@ def penaltyMatrix2D(
 
             rv[dv].i[i * nj : (i + 1) * nj] = i
             rv[dv].j[i * nj : (i + 1) * nj] = (
-                ((uspan - uorder + np.arange(nu)) % nu_total) * nv_total
-                + ((vspan - vorder + np.arange(nv)) % nv_total)[:, np.newaxis]
+                ((uspan - uorder + int(uperiodic) + np.arange(nu)) % nu_total)
+                * nv_total
+                + ((vspan - vorder + int(vperiodic) + np.arange(nv)) % nv_total)[
+                    :, np.newaxis
+                ]
             ).ravel()
             rv[dv].v[i * nj : (i + 1) * nj] = (
                 utemp[du, :] * vtemp[dv, :, np.newaxis]

tests/test_nurbs.py
@@ -12,6 +12,7 @@ from cadquery.occ_impl.nurbs import (
     periodicApproximate,
     periodicLoft,
     loft,
+    array2vec,
 )
 
 from cadquery.func import circle, torus
@@ -277,7 +278,8 @@ def test_approximate():
 
 def test_periodic_approximate():
 
-    pts_ = circle(1).sample(100)[0]
+    circ = circle(1)
+    pts_ = circ.sample(100)[0]
     pts = np.array([list(p) for p in pts_])
 
     crv = periodicApproximate(pts)
@@ -285,6 +287,12 @@ def test_periodic_approximate():
 
     assert e.isValid()
     assert e.Length() == approx(2 * np.pi)
+
+    # check params
+    us0 = circ.params(pts_)
+    us1 = e.params(pts_)
+
+    assert np.allclose(us0, np.array(us1) * 2 * np.pi)
 
     # multiple approximate
     crvs = periodicApproximate([pts, pts])
@@ -352,5 +360,18 @@ def test_approximate2D(lam, penalty):
 
     f = surf.face()
 
+    # general sanity checks
     assert f.isValid()
-    assert f.Area() == approx(t.Area(), rel=1e-4)
+    assert f.Area() == approx(t.Area(), rel=1e-3)
+
+    # check the stability of the parameters
+    us0 = us[None, :].repeat(len(vs), 0).ravel()
+    vs0 = vs[:, None].repeat(len(us), 1).ravel()
+
+    us2, vs2 = f.params(array2vec(pts))
+
+    delta_u = np.array(us2 - us0)
+    delta_v = np.array(vs2 - vs0)
+
+    assert np.allclose(np.where(delta_u >= 1, delta_u, 0) % 1, 0)
+    assert np.allclose(np.where(delta_v >= 1, delta_v, 0) % 1, 0)