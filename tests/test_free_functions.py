5d34edd89bc28c471c200f574f2f2de90cc8e892
adam-urbanczyk <13981538+adam-urbanczyk@users.noreply.github.com>
2026-05-09 12:25:45 +0200

    Trigger different path

--- tests/test_free_functions.py
+++ tests/test_free_functions.py
@@ -148,7 +148,7 @@ def test__shape_to_faces_shells():
 
     # bed weather tests
     with raises(ValueError):
-        _shape_to_faces_shells(compound(vertex(0, 0, 0)))
+        _shape_to_faces_shells(compound(vertex(1, 0, 0), vertex(0, 0, 0)))
 
     with raises(ValueError):
         _shape_to_faces_shells(vertex(0, 0, 0))
