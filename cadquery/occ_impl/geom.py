c2c823e71968296d524906760b2d4750b44bf638
AU <adam-urbanczyk@users.noreply.github.com>
2026-08-29 18:31:57 +0200

    Back to using operators
    Co-authored-by: AU <adam-urbanczyk@users.noreply.github.com>

--- cadquery/occ_impl/geom.py
+++ cadquery/occ_impl/geom.py
@@ -360,7 +360,7 @@ class Matrix:
         new = gp_Trsf()
         new.SetRotation(direction, angle)
 
-        self.wrapped = self.wrapped.Multiplied(gp_GTrsf(new))
+        self.wrapped = self.wrapped * gp_GTrsf(new)
 
     def inverse(self) -> "Matrix":
 
@@ -1142,7 +1142,7 @@ class Location(object):
 
     def __mul__(self, other: "Location") -> "Location":
 
-        return Location(self.wrapped.Multiplied(other.wrapped))
+        return Location(self.wrapped * other.wrapped)
 
     def __pow__(self, exponent: int) -> "Location":
 
